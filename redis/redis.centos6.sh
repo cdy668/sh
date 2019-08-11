c5a62e401b95c81b568ac54634f1308ad8885bf4
Jasper <52143094+0vJ6@users.noreply.github.com>
2019-08-11 22:54:44 +0800

    Update redis.centos6.sh

--- redis/redis.centos6.sh
+++ redis/redis.centos6.sh
@@ -20,7 +20,7 @@ init(){
     dir="$base_dir/var/lib/redis"
     dbfilename="dump_$port.rdb"
     appendfilename="appendonly_$port.aof"
-    yum -y install openssl-devel make gcc
+    yum -y install openssl-devel make gcc libyaml
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     curl -sSL https://get.rvm.io | bash -s stable
     source /etc/profile.d/rvm.sh

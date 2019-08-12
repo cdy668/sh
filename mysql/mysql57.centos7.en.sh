6fcd9ed9ea436f6c1159d7cd1f52c5b33269511e
Jasper <52143094+0vJ6@users.noreply.github.com>
2019-08-12 13:02:06 +0800

    Update mysql57.centos7.en.sh

--- mysql/mysql57.centos7.en.sh
+++ mysql/mysql57.centos7.en.sh
@@ -15,7 +15,7 @@ init(){
     rm -f /usr/bin/mysql*
     base_dir="/home/mysql"
     config_file="$base_dir/usr/local/mysql/my.cnf"
-    yum -y install libaio wget bzip2 numactl
+    yum -y install libaio wget bzip2 numactl numactl-libs
     useradd -r -s /bin/false -m -d $base_dir -c 'MySQL Server' mysql
     cd $base_dir
     wget "https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-$1-linux-glibc2.12-x86_64.tar.gz"

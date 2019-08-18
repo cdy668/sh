1b2bf4894cebcbd5b252fbbcf315f7a545ac5edf
Jasper <52143094+0vJ6@users.noreply.github.com>
2019-08-18 17:27:15 +0800

    Update nginx.centos7.sh

--- nginx/nginx.centos7.sh
+++ nginx/nginx.centos7.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # nginx script file example
-#     ./nginx.centos7.sh $1 $2
-#     ./nginx.centos7.sh init 1.16.0
+#     ./nginx.centos7.sh $1 $2 $3
+#     ./nginx.centos7.sh init stable 1.16.0
+#     ./nginx.centos7.sh init mainline 1.17.3
 
 init(){
     systemctl stop nginx
@@ -13,38 +14,38 @@ init(){
     rm -fr /var/lib/yum/repos/x86_64/7/nginx
     rm -fr /var/log/nginx
     rm -f /etc/yum.repos.d/nginx.repo
-    rpm -e gpg-pubkey-7bd9bf62-5762b5f8
-    echo "[nginx]" > /etc/yum.repos.d/nginx.repo
-    echo "name=nginx repo" >> /etc/yum.repos.d/nginx.repo
-    echo "baseurl=https://nginx.org/packages/centos/7/x86_64/" >> /etc/yum.repos.d/nginx.repo
-    echo "gpgcheck=0" >> /etc/yum.repos.d/nginx.repo
-    echo "enabled=1" >> /etc/yum.repos.d/nginx.repo
+    # rpm -q gpg-pubkey --qf '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'
+    # rpm -e gpg-pubkey-7bd9bf62-5762b5f8
+    yum -y install yum-utils
+    echo "[nginx-stable]"                                              > /etc/yum.repos.d/nginx.repo
+    echo "name=nginx stable repo"                                      >> /etc/yum.repos.d/nginx.repo
+    echo "baseurl=http://nginx.org/packages/centos/7/x86_64/"          >> /etc/yum.repos.d/nginx.repo
+    echo "gpgcheck=1"                                                  >> /etc/yum.repos.d/nginx.repo
+    echo "enabled=0"                                                   >> /etc/yum.repos.d/nginx.repo
+    echo "gpgkey=https://nginx.org/keys/nginx_signing.key"             >> /etc/yum.repos.d/nginx.repo
+    echo "[nginx-mainline]"                                            >> /etc/yum.repos.d/nginx.repo
+    echo "name=nginx mainline repo"                                    >> /etc/yum.repos.d/nginx.repo
+    echo "baseurl=http://nginx.org/packages/mainline/centos/7/x86_64/" >> /etc/yum.repos.d/nginx.repo
+    echo "gpgcheck=1"                                                  >> /etc/yum.repos.d/nginx.repo
+    echo "enabled=0"                                                   >> /etc/yum.repos.d/nginx.repo
+    echo "gpgkey=https://nginx.org/keys/nginx_signing.key"             >> /etc/yum.repos.d/nginx.repo
     chmod 644 /etc/yum.repos.d/nginx.repo
-    # To list all imported signing archive keys run:
-    #     rpm --import https://nginx.org/packages/keys/nginx_signing.key
-    # You the above list to remove any unwanted archive signing keys. This can be done by using rpm command:
-    #     rpm -e gpg-pubkey-7bd9bf62-5762b5f8
-    rpm --import https://nginx.org/packages/keys/nginx_signing.key
-    if [[ "$1" =~ ^1.10.0$|^1.10.1$|^1.10.2$|^1.10.3$|^1.12.0$|^1.12.1$|^1.16.0$|^1.16.1$|^1.8.0$|^1.8.1$ ]]; then
-        yum -y install https://nginx.org/packages/centos/7/x86_64/RPMS/nginx-$1-1.el7.ngx.x86_64.rpm
-    elif [[ "$1" =~ ^1.12.2$|^1.14.0$|^1.14.1$|^1.14.2$ ]]; then
-        yum -y install https://nginx.org/packages/centos/7/x86_64/RPMS/nginx-$1-1.el7_4.ngx.x86_64.rpm
-    else
-        echo "Version list"
-        echo "    1.16.1"
-        echo "    1.16.0"
-        echo "    1.14.2"
-        echo "    1.14.1"
-        echo "    1.14.0"
-        echo "    1.12.2"
-        echo "    1.12.1"
-        echo "    1.12.0"
-        echo "    1.10.3"
-        echo "    1.10.2"
-        echo "    1.10.1"
-        echo "    1.10.0"
-        echo "    1.8.1"
-        echo "    1.8.0"
+    if [[ "$1" = "stable" ]] ; then
+        yum-config-manager --enable nginx-stable
+        special_name="$(curl -sSL http://nginx.org/packages/centos/7/x86_64/RPMS/ | awk -F "\"" '{print $2}' | grep "^nginx-$2-1.*_4.ngx.x86_64.rpm" | awk -F "-" '{print $2}')"
+        if [[ -n "$(echo "$special_name" | grep -o "$2")" ]];then
+            yum -y install https://nginx.org/packages/centos/7/x86_64/RPMS/nginx-$2-1.el7_4.ngx.x86_64.rpm
+        else
+            yum -y install https://nginx.org/packages/centos/7/x86_64/RPMS/nginx-$2-1.el7.ngx.x86_64.rpm
+        fi
+    elif [[ "$1" = "mainline" ]]; then
+        yum-config-manager --enable nginx-mainline
+        special_name="$(curl -sSL https://nginx.org/packages/mainline/centos/7/x86_64/RPMS/ | awk -F "\"" '{print $2}' | grep "^nginx-$2-1.*_4.ngx.x86_64.rpm" | awk -F "-" '{print $2}')"
+        if [[ -n "$(echo "$special_name" | grep -o "$2")" ]];then
+            yum -y install https://nginx.org/packages/mainline/centos/7/x86_64/RPMS/nginx-$2-1.el7_4.ngx.x86_64.rpm
+        else
+            yum -y install https://nginx.org/packages/mainline/centos/7/x86_64/RPMS/nginx-$2-1.el7.ngx.x86_64.rpm
+        fi
     fi
     systemctl restart nginx
     systemctl status nginx
@@ -53,6 +54,6 @@ init(){
 
 case $1 in
     init)
-        init "$2"
+        init "$2" "$3"
     ;;
 esac

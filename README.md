1f6f2bea51727067d6e6c24771aa32bdae6908a0
Jasper <52143094+0vJ6@users.noreply.github.com>
2019-09-20 20:09:03 +0800

    Update
    add vsftpd install scripts
    update redis install scripts

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
     curl -sSL https://0vj6.github.io/sh/mysql/mysql57.ubuntu.18.04.en.sh | bash -s init 5.7.26
     
     curl -sSL https://0vj6.github.io/sh/mysql/mysql80.ubuntu.18.04.en.sh | bash -s init 8.0.16
-    
+
 ## nginx
 
     curl -sSL https://0vj6.github.io/sh/nginx/nginx.centos7.sh | bash -s init stble 1.16.1
@@ -49,7 +49,7 @@
     curl -sSL https://0vj6.github.io/sh/nginx/nginx.centos6.sh | bash -s init mainline 1.13.12
     
     curl -sSL https://0vj6.github.io/sh/nginx/nginx.ubuntu.18.04.sh | bash -s init mainline 1.13.12
-    
+
 ## php
     
     curl -sSL https://0vj6.github.io/sh/php/php.centos7.sh | bash -s init php56
@@ -59,7 +59,7 @@
     curl -sSL https://0vj6.github.io/sh/php/php.centos6.sh | bash -s init php56
 
     curl -sSL https://0vj6.github.io/sh/php/php.centos6.sh | bash -s init php73
-    
+
 ## redis
 
     curl -sSL https://0vj6.github.io/sh/redis/redis.centos7.sh | bash -s init 4.0.14 127.0.0.1
@@ -73,7 +73,13 @@
     curl -sSL https://0vj6.github.io/sh/redis/redis.ubuntu.18.04.sh | bash -s init 4.0.14 127.0.0.1
     
     curl -sSL https://0vj6.github.io/sh/redis/redis.ubuntu.18.04.sh | bash -s init 5.0.5 127.0.0.1
+
+## vsftpd
     
+    curl -sSL https://0vj6.github.io/sh/vsftpd/vsftpd.centos7.sh | bash -s init
+    
+    curl -sSL https://0vj6.github.io/sh/vsftpd/vsftpd.centos6.sh | bash -s init
+
 ## zabbix
     
     curl -sSL https://0vj6.github.io/sh/zabbix/zabbix4.centos7.sh | bash -s init agent 4.0
---
+++ vsftpd/vsftpd.centos7.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# vsftpd script file example
+#     ./vsftpd.centos7.sh $1
+#     ./vsftpd.centos7.sh init
+
+init(){
+    systemctl stop vsftpd
+    systemctl status vsftpd
+    yum -y autoremove vsftpd
+    userdel -r administrator
+    rm -fr /etc/vsftpd
+    rm -f /var/log/vsftpd.log*
+    rm -f /var/log/xferlog*
+    setenforce 0
+    yum -y install vsftpd
+    sed -i "s/anonymous_enable=YES/anonymous_enable=NO/"                                 /etc/vsftpd/vsftpd.conf
+    sed -i "s/#anon_upload_enable=YES/anon_upload_enable=NO/"                            /etc/vsftpd/vsftpd.conf
+    sed -i "s/#anon_mkdir_write_enable=NO/anon_mkdir_write_enable=NO/"                   /etc/vsftpd/vsftpd.conf
+    sed -i "s/#xferlog_file=\/var\/log\/xferlog/xferlog_file=\/var\/log\/xferlog/"       /etc/vsftpd/vsftpd.conf
+    sed -i "s/#idle_session_timeout=600/idle_session_timeout=600/"                       /etc/vsftpd/vsftpd.conf
+    sed -i "s/#data_connection_timeout=120/data_connection_timeout=120/"                 /etc/vsftpd/vsftpd.conf
+    sed -i "s/#chroot_local_user=YES/chroot_local_user=YES/"                             /etc/vsftpd/vsftpd.conf
+    echo "allow_writeable_chroot=YES"                                                 >> /etc/vsftpd/vsftpd.conf
+    echo "guest_enable=YES"                                                           >> /etc/vsftpd/vsftpd.conf
+    echo "guest_username=administrator"                                               >> /etc/vsftpd/vsftpd.conf
+    echo "user_config_dir=/etc/vsftpd/virtual_user_conf"                              >> /etc/vsftpd/vsftpd.conf
+    echo "virtual_use_local_privs=YES"                                                >> /etc/vsftpd/vsftpd.conf
+    echo "dual_log_enable=YES"                                                        >> /etc/vsftpd/vsftpd.conf
+    echo "vsftpd_log_file=/var/log/vsftpd.log"                                        >> /etc/vsftpd/vsftpd.conf
+    echo -e "administrator\n$(openssl rand -base64 20)" > /etc/vsftpd/virtual_user
+    db_load -T -t hash -f /etc/vsftpd/virtual_user /etc/vsftpd/virtual_user.db
+    chmod 600 /etc/vsftpd/virtual_user.db
+    useradd -s /usr/sbin/nologin -m -d /home/administrator -c 'ftp virtual user' administrator
+    mv /etc/pam.d/vsftpd /etc/pam.d/vsftpd.bak
+    echo "auth    required /usr/lib64/security/pam_userdb.so db=/etc/vsftpd/virtual_user" > /etc/pam.d/vsftpd
+    echo "account required /usr/lib64/security/pam_userdb.so db=/etc/vsftpd/virtual_user" >> /etc/pam.d/vsftpd
+    mkdir -p /etc/vsftpd/virtual_user_conf
+    echo "local_root=/home/administrator"                > /etc/vsftpd/virtual_user_conf/administrator
+    echo "write_enable=YES"                              >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "anonymous_enable=NO"                           >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "local_umask=022"                               >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "anon_upload_enable=YES"                        >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "anon_mkdir_write_enable=YES"                   >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "anon_other_write_enable=YES"                   >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "anon_world_readable_only=YES"                  >> /etc/vsftpd/virtual_user_conf/administrator
+    echo "download_enable=YES"                           >> /etc/vsftpd/virtual_user_conf/administrator
+    systemctl enable vsftpd
+    systemctl start vsftpd
+    systemctl status vsftpd
+    cat /etc/vsftpd/virtual_user
+    echo "don't forget delete this file /etc/vsftpd/virtual_user"
+}
+
+case $1 in
+    init)
+        init
+    ;;
+esac

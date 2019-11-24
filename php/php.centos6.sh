7695861ea5941bb9fbcdd12795cbd343be5ffb82
0vJ6 <52143094+0vJ6@users.noreply.github.com>
2019-11-24 16:34:00 +0800

    Update

--- php/php.centos6.sh
+++ php/php.centos6.sh
@@ -4,14 +4,31 @@
 #     ./php.centos6.sh init php73
 
 init(){
-    yum -y remove php* remi-release
+    yum -y remove php*-* remi-release
     rm -fr /etc/php*
     rm -fr /opt/remi*
     rm -fr /usr/bin/php*
+    rm -fr /etc/yum.repos.d/remi*
     yum -y install yum-utils
     yum -y install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
     yum-config-manager --enable remi-$1
-    yum -y install $1 $1-php-fpm $1-php-mysqlnd $1-php-pecl-redis4 $1-php-pecl-memcache
+    yum -y install $1 \
+                   $1-php-fpm \
+                   $1-php-cli \
+                   $1-php-common \
+                   $1-runtime \
+                   $1-php-mysqlnd \
+                   $1-php-pecl-redis4 \
+                   $1-php-pecl-memcache \
+                   $1-php-pecl-ssh2 \
+                   $1-php-bcmath \
+                   $1-php-gd \
+                   $1-php-ldap \
+                   $1-php-mbstring \
+                   $1-php-pdo \
+                   $1-php-xml \
+                   $1-php-json \
+                   $1-php-pecl-igbinary
     ln -s /usr/bin/$1 /usr/bin/php
     php --ini
     php -v
--- php/php.centos7.sh
+++ php/php.centos7.sh
@@ -4,7 +4,7 @@
 #     ./php.centos7.sh init php73
 
 init(){
-    yum -y autoremove php* remi-release
+    yum -y autoremove php*-* remi-release
     rm -fr /etc/yum.repos.d/remi*
     rm -fr /etc/php*
     rm -fr /etc/opt/remi*
@@ -18,7 +18,23 @@ init(){
     yum -y install yum-utils
     yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
     yum-config-manager --enable remi-$1
-    yum -y install $1 $1-php-fpm $1-php-mysqlnd $1-php-pecl-redis4 $1-php-pecl-memcache
+    yum -y install $1 \
+                   $1-php-fpm \
+                   $1-php-cli \
+                   $1-php-common \
+                   $1-runtime \
+                   $1-php-mysqlnd \
+                   $1-php-pecl-redis4 \
+                   $1-php-pecl-memcache \
+                   $1-php-pecl-ssh2 \
+                   $1-php-bcmath \
+                   $1-php-gd \
+                   $1-php-ldap \
+                   $1-php-mbstring \
+                   $1-php-pdo \
+                   $1-php-xml \
+                   $1-php-json \
+                   $1-php-pecl-igbinary                
     ln -s /usr/bin/$1 /usr/bin/php
     php --ini
     php -v

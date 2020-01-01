07fca326427aced9d76ff9c856a7e463e9067492
IvPIJCnnEXUZbczNDisX <52143094+0vJ6@users.noreply.github.com>
2020-01-01 12:44:51 +0800

    Update php.centos6.sh

--- php/php.centos6.sh
+++ php/php.centos6.sh
@@ -18,9 +18,22 @@ init(){
                    $1-php-common \
                    $1-runtime \
                    $1-php-mysqlnd \
-                   $1-php-pecl-redis4 \
-                   $1-php-pecl-memcache \
-                   $1-php-pecl-ssh2 \
+                   $1-php-curl \
+                   $1-php-zip \
+                   $1-php-redis \
+                   $1-php-fileinfo \
+                   $1-php-intl \
+                   $1-php-opcache \
+                   $1-php-process \
+                   $1-php-pear \
+                   $1-php-devel \
+                   $1-php-openssl \
+                   $1-php-embedded \
+                   $1-php-imap \
+                   $1-php-odbc \
+                   $1-php-xmlrpc \
+                   $1-php-snmp \
+                   $1-php-soap \
                    $1-php-bcmath \
                    $1-php-gd \
                    $1-php-ldap \
@@ -28,8 +41,15 @@ init(){
                    $1-php-pdo \
                    $1-php-xml \
                    $1-php-json \
-                   $1-php-pecl-igbinary
+                   $1-php-pecl-redis4 \
+                   $1-php-pecl-memcache \
+                   $1-php-pecl-ssh2 \
+                   $1-php-pecl-mcrypt \
+                   $1-php-pecl-mongodb \
+                   $1-php-pecl-swoole4 \
+                   $1-php-pecl-igbinary 
     ln -s /usr/bin/$1 /usr/bin/php
+    ln -s /opt/remi/$1/root/usr/bin/phpize /usr/bin/phpize
     php --ini
     php -v
     php -m

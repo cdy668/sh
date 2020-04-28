eaab8fc7d02b2a04a424ae11ecbe1c0a37224b11
root <root@DESKTOP-K7ISFLJ.localdomain>
2020-04-28 20:03:20 +0800

    Update for init server

--- init/init.centos7.sh
+++ init/init.centos7.sh
@@ -7,152 +7,170 @@
 #     https://remotedesktop.google.com/headless
 
 init_desktop(){
-    sudo yum -y update
-    sudo yum -y install epel-release
-    sudo yum -y install gcc \
-                        gcc-c++ \
-                        vim \
-                        git \
-                        mlocate \
-                        iftop \
-                        sysstat \
-                        net-tools \
-                        dos2unix \
-                        tree \
-                        wget \
-                        curl \
-                        lrzsz \
-                        openssh-server \
-                        zip \
-                        unzip \
-                        make \
-                        traceroute \
-                        mtr \
-                        telnet \
-                        python2-pip \
-                        python3-pip \
-                        python*virtualenv \
-                        redhat-lsb \
-                        yum-utils \
-                        ansible \
-                        cockpit \
-                        cockpit-ws \
-                        cockpit-dashboard \
-                        cockpit-storaged \
-                        chkconfig \
-                        tcping \
-                        ipset \
-                        ethtool
+    yum -y update
+    yum -y install epel-release
+    yum -y install gcc \
+                   gcc-c++ \
+                   vim \
+                   git \
+                   mlocate \
+                   iftop \
+                   sysstat \
+                   net-tools \
+                   dos2unix \
+                   tree \
+                   wget \
+                   curl \
+                   lrzsz \
+                   openssh-server \
+                   zip \
+                   unzip \
+                   make \
+                   traceroute \
+                   mtr \
+                   telnet \
+                   python2-pip \
+                   python3-pip \
+                   python*virtualenv \
+                   redhat-lsb \
+                   yum-utils \
+                   ansible \
+                   cockpit \
+                   cockpit-ws \
+                   cockpit-dashboard \
+                   cockpit-storaged \
+                   chkconfig \
+                   tcping \
+                   ipset \
+                   ethtool
     sudo wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo
-    sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-    sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-    sudo yum -y install extundelete google-authenticator httping ansible
-    sudo yum -y groupinstall "GNOME Desktop"
+    yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+    yum -y install extundelete google-authenticator httping ansible
+    yum -y groupinstall "GNOME Desktop"
     sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
-    sudo yum -y install gnome-tweak-tool \
-                        firewalld-config \
-                        filezilla \
-                        gimp \
-                        qbittorrent \
-                        thunderbird \
-                        dconf-editor \
-                        libreoffice \
-                        remmina \
-                        firefox \
-                        vino \
-                        fcitx \
-                        fcitx-pinyin \
-                        fcitx-data \
-                        fcitx-gtk2 \
-                        fcitx-gtk3 \
-                        fcitx-libs \
-                        fcitx-configtool
-    # sudo yum -y install chrome-remote-desktop
-    sudo yum -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-    sudo yum -y install https://repo.skype.com/latest/skypeforlinux-64.rpm
+    yum -y install gnome-tweak-tool \
+                   firewalld-config \
+                   filezilla \
+                   gimp \
+                   qbittorrent \
+                   thunderbird \
+                   dconf-editor \
+                   libreoffice \
+                   remmina \
+                   firefox \
+                   vino \
+                   fcitx \
+                   fcitx-pinyin \
+                   fcitx-data \
+                   fcitx-gtk2 \
+                   fcitx-gtk3 \
+                   fcitx-libs \
+                   fcitx-configtool
+    # yum -y install chrome-remote-desktop
+    yum -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+    yum -y install https://repo.skype.com/latest/skypeforlinux-64.rpm
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    sudo yum check-update
-    sudo yum -y install code
-    sudo yum -y install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-    sudo yum -y install vlc vlc-core python-vlc npapi-vlc exfat-utils fuse-exfat
+    yum check-update
+    yum -y install code
+    yum -y install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+    yum -y install vlc vlc-core python-vlc npapi-vlc exfat-utils fuse-exfat
     curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
-    sudo yum -y install powershell
+    yum -y install powershell
 }
 
 init_server(){
-    sudo yum -y update
-    sudo yum -y install epel-release
-    sudo yum -y install gcc \
-                        gcc-c++ \
-                        vim \
-                        git \
-                        mlocate \
-                        iftop \
-                        sysstat \
-                        net-tools \
-                        dos2unix \
-                        tree \
-                        wget \
-                        curl \
-                        lrzsz \
-                        openssh-server \
-                        zip \
-                        unzip \
-                        make \
-                        traceroute \
-                        mtr \
-                        telnet \
-                        python2-pip \
-                        python3-pip \
-                        python*virtualenv \
-                        redhat-lsb \
-                        yum-utils \
-                        ansible \
-                        cockpit \
-                        cockpit-ws \
-                        cockpit-dashboard \
-                        cockpit-storaged \
-                        chkconfig \
-                        tcping \
-                        ipset \
-                        ethtool
+    yum -y update
+    yum -y install epel-release
+    yum -y install gcc \
+                   gcc-c++ \
+                   vim \
+                   git \
+                   mlocate \
+                   iftop \
+                   sysstat \
+                   net-tools \
+                   dos2unix \
+                   tree \
+                   wget \
+                   curl \
+                   lrzsz \
+                   openssh-server \
+                   zip \
+                   unzip \
+                   make \
+                   traceroute \
+                   mtr \
+                   telnet \
+                   python2-pip \
+                   python3-pip \
+                   python*virtualenv \
+                   redhat-lsb \
+                   yum-utils \
+                   ansible \
+                   cockpit \
+                   cockpit-ws \
+                   cockpit-dashboard \
+                   cockpit-storaged \
+                   chkconfig \
+                   tcping \
+                   ipset \
+                   ethtool \
+                   google-authenticator
 }
 
 init_iptables(){
-    sudo yum -y update iptables-services
+    yum -y install iptables-services wget
 }
 
 init_firewalld(){
-    sudo yum -y update firewalld
+    yum -y install firewalld wget
 }
 
 init_sshd(){
-    sudo yum -y update openssh-server
+    yum -y install openssh-server wget
+    mv /etc/ssh/sshd_config /etc/ssh/sshd_config.$(date +"%Y%m%d%H%M%S")
+    wget -O /etc/ssh/sshd_config https://0vj6.github.io/sh/init/sshd/sshd_config
+    chmod 600 /etc/ssh/sshd_config
+    systemctl restart sshd && systemctl status sshd
 }
 
 init_ulimit(){
-    echo "not ready"
+    yum -y install wget
 }
 
 init_sudoers(){
-    echo "not ready"
+    yum -y install wget
 }
 
 init_ldconfig(){
-    echo "not ready"
+    yum -y install wget
 }
 
 init_logrotate(){
-    echo "not ready"
+    yum -y install wget
+    mv /etc/logrotate.d/syslog /etc/logrotate.d/syslog.$(date +"%Y%m%d%H%M%S")
+    mv /etc/logrotate.d/yum /etc/logrotate.d/yum.$(date +"%Y%m%d%H%M%S")
+    wget -O /etc/logrotate.d/syslog https://0vj6.github.io/sh/init/logrotate/syslog
+    wget -O /etc/logrotate.d/yum https://0vj6.github.io/sh/init/logrotate/yum
+    chmod 644 /etc/logrotate.d/syslog
+    chmod 644 /etc/logrotate.d/yum
 }
 
 init_vim(){
-    echo "not ready"
+    yum -y install vim wget
+    mv ~/.vimrc ~/.vimrc.$(date +"%Y%m%d%H%M%S")
+    wget -O ~/.vimrc https://0vj6.github.io/sh/init/vim/.vimrc
+    chmod 600 ~/.vimrc
 }
 
 init_rsyslog(){
-    echo "not ready"
+    yum -y install rsyslog wget
+    mv /etc/rsyslog.conf /etc/rsyslog.conf.$(date +"%Y%m%d%H%M%S")
+    wget -O /etc/rsyslog.conf https://0vj6.github.io/sh/init/rsyslog/rsyslog.conf
+    chmod 644 /etc/rsyslog.conf
+    systemctl restart rsyslog && systemctl status rsyslog
 }
 
 case $1 in
--- init/sshd/README.md
+++ init/sshd/README.md
@@ -1,1 +1,43 @@
 # sshd
+## Port
+    定义sshd服务的端口号，默认为22
+## SyslogFacility
+    定义sshd服务的日志存储位置，默认为local1
+
+    同时需要rsyslog.conf做出调整
+    1、追加内容到/etc/rsyslog.conf
+        # Save sshd messages also to sshd.log
+        local1.*                                                /var/log/sshd.log
+    2、追加内容到/etc/logrotate.d/syslog
+        /var/log/sshd.log
+    3、重启sshd、rsyslog
+        systemctl restart sshd rsyslog && systemctl status sshd rsyslog
+## LogLevel
+    定义sshd服务的日志级别，默认为INFO
+## LoginGraceTime
+    定义sshd服务的登录过程的超时时间，单位可以是s（秒），也可以是m（分钟）
+    默认为30s，超过这个时间，服务器会断开当前建立的连接
+## PermitRootLogin
+    定义系统root用户是否可以远程登录sshd服务，默认为yes，推荐设置为no
+## StrictModes
+    定义sshd服务在接收登录请求之前是否检查用户主目录和rhosts文件的权限和所有权，默认为yes
+## MaxAuthTries
+    定义sshd服务的尝试登录次数，默认为2
+    第一次尝试登录失败，还可以进行第二次尝试登录，如果第二次尝试登录失败，会直接被sshd服务拒绝登录
+    可以遏制hydra等暴力破解工具对sshd服务的恶意攻击
+## MaxSessions
+    定义sshd服务可以接受来自同一个IP地址的最大连接数，默认为10
+## PubkeyAuthentication
+    定义sshd服务是否允许使用公钥认证，默认为yes
+    可以同时保持sshd服务的密码认证，二者不会冲突
+    推荐使用公钥认证方式登录的时候，关闭掉密码认证方式，可以遏制hydra等暴力破解工具对sshd服务的恶意攻击
+    私钥绝对不能被泄露，否则只能更新密钥对
+## PermitEmptyPasswords
+    定义sshd服务是否允许使用空密码，默认为no
+## PasswordAuthentication
+    定义sshd服务是否允许使用密码认证，默认为yes
+    如果一定要开启密码认证，建议配合谷歌身份验证器（google-authenticator）来完成二次身份认证，可以防止其他人仅凭密码就可以登录系统
+## Subsystem	sftp	/usr/libexec/openssh/sftp-server
+    定义sshd服务是否开启sftp-server功能，该配置默认被注释
+    # Subsystem	sftp	/usr/libexec/openssh/sftp-server
+

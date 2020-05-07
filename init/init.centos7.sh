d864c92a28a48d35a863eadc11ed7fba25fc0e13
robot <robot@localhost>
2020-05-07 17:37:02 +0800

    Fix some problems

--- init/init.centos7.sh
+++ init/init.centos7.sh
@@ -80,6 +80,12 @@ init_desktop(){
     yum -y install vlc vlc-core python-vlc npapi-vlc exfat-utils fuse-exfat
     curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
     yum -y install powershell
+    sudo git config --global user.name "root"
+    sudo git config --global user.email root@localhost
+    sudo git config --list --show-origin
+    git config --global user.name "$USER"
+    git config --global user.email "$USER"@localhost
+    git config --list --show-origin
 }
 
 init_server(){
@@ -122,6 +128,12 @@ init_server(){
                    extundelete \
                    httping \
                    nmap
+    sudo git config --global user.name "root"
+    sudo git config --global user.email root@localhost
+    sudo git config --list --show-origin
+    git config --global user.name "$USER"
+    git config --global user.email "$USER"@localhost
+    git config --list --show-origin
 }
 
 init_iptables(){
@@ -130,7 +142,7 @@ init_iptables(){
     systemctl disable firewalld
     systemctl status firewalld
     mv /etc/sysconfig/iptables /etc/sysconfig/iptables.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/sysconfig/iptables https://0vj6.github.io/sh/init/iptables/iptables
+    wget -O /etc/sysconfig/iptables https://0vj6.github.io/sh/init/centos7/iptables/iptables
     chmod 600 /etc/sysconfig/iptables
     wget -O /etc/sysconfig/cloudflare_ips-v4 https://www.cloudflare.com/ips-v4
     chmod 600 /etc/sysconfig/cloudflare_ips-v4
@@ -172,7 +184,7 @@ init_firewalld(){
     systemctl disable iptables
     systemctl status iptables
     mv /etc/firewalld/zones/public.xml /etc/firewalld/zones/public.xml.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/firewalld/zones/public.xml https://0vj6.github.io/sh/init/firewalld/public.xml
+    wget -O /etc/firewalld/zones/public.xml https://0vj6.github.io/sh/init/centos7/firewalld/public.xml
     chmod 644 /etc/firewalld/zones/public.xml
     systemctl restart firewalld && systemctl status firewalld && systemctl enable firewalld
 }
@@ -180,7 +192,7 @@ init_firewalld(){
 init_sshd(){
     yum -y install openssh-server wget
     mv /etc/ssh/sshd_config /etc/ssh/sshd_config.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/ssh/sshd_config https://0vj6.github.io/sh/init/sshd/sshd_config
+    wget -O /etc/ssh/sshd_config https://0vj6.github.io/sh/init/centos7/sshd/sshd_config
     chmod 600 /etc/ssh/sshd_config
     systemctl restart sshd && systemctl status sshd && systemctl enable sshd
 }
@@ -188,11 +200,11 @@ init_sshd(){
 init_ulimit(){
     yum -y install wget
     mv /etc/security/limits.conf /etc/security/limits.conf.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/security/limits.conf https://0vj6.github.io/sh/init/ulimit/limits.conf
+    wget -O /etc/security/limits.conf https://0vj6.github.io/sh/init/centos7/ulimit/limits.conf
     chmod 644 /etc/security/limits.conf
     mv /etc/security/limits.d/20-nproc.conf /etc/security/limits.d/20-nproc.conf.$(date +"%Y%m%d%H%M%S")
     mv /etc/sysctl.conf /etc/sysctl.conf.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/sysctl.conf https://0vj6.github.io/sh/init/ulimit/sysctl.conf
+    wget -O /etc/sysctl.conf https://0vj6.github.io/sh/init/centos7/ulimit/sysctl.conf
     chmod 644 /etc/sysctl.conf
     echo 819200 > /proc/sys/fs/inotify/max_user_watches
     sysctl -p
@@ -201,7 +213,7 @@ init_ulimit(){
 init_sudoers(){
     yum -y install wget
     mv /etc/sudoers /etc/sudoers.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/sudoers https://0vj6.github.io/sh/init/sudoers/sudoers
+    wget -O /etc/sudoers https://0vj6.github.io/sh/init/centos7/sudoers/sudoers
     chmod 440 /etc/sudoers
 }
 
@@ -214,7 +226,7 @@ init_ldconfig(){
 init_selinux(){
     yum -y install wget
     mv /etc/selinux/config /etc/selinux/config.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/selinux/config https://0vj6.github.io/sh/init/selinux/config
+    wget -O /etc/selinux/config https://0vj6.github.io/sh/init/centos7/selinux/config
     chmod 644 /etc/selinux/config
     setenforce 0
     getenforce
@@ -224,8 +236,8 @@ init_logrotate(){
     yum -y install wget
     mv /etc/logrotate.d/syslog /etc/logrotate.d/syslog.$(date +"%Y%m%d%H%M%S")
     mv /etc/logrotate.d/yum /etc/logrotate.d/yum.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/logrotate.d/syslog https://0vj6.github.io/sh/init/logrotate/syslog
-    wget -O /etc/logrotate.d/yum https://0vj6.github.io/sh/init/logrotate/yum
+    wget -O /etc/logrotate.d/syslog https://0vj6.github.io/sh/init/centos7/logrotate/syslog
+    wget -O /etc/logrotate.d/yum https://0vj6.github.io/sh/init/centos7/logrotate/yum
     chmod 644 /etc/logrotate.d/syslog
     chmod 644 /etc/logrotate.d/yum
 }
@@ -233,14 +245,14 @@ init_logrotate(){
 init_vim(){
     yum -y install vim wget
     mv ~/.vimrc ~/.vimrc.$(date +"%Y%m%d%H%M%S")
-    wget -O ~/.vimrc https://0vj6.github.io/sh/init/vim/vimrc
+    wget -O ~/.vimrc https://0vj6.github.io/sh/init/centos7/vim/vimrc
     chmod 600 ~/.vimrc
 }
 
 init_rsyslog(){
     yum -y install rsyslog wget
     mv /etc/rsyslog.conf /etc/rsyslog.conf.$(date +"%Y%m%d%H%M%S")
-    wget -O /etc/rsyslog.conf https://0vj6.github.io/sh/init/rsyslog/rsyslog.conf
+    wget -O /etc/rsyslog.conf https://0vj6.github.io/sh/init/centos7/rsyslog/rsyslog.conf
     chmod 644 /etc/rsyslog.conf
     echo "* * * * * chattr +u /var/log/sshd*"              >> /var/spool/cron/root
     echo "* * * * * chattr +u /var/log/boot*"              >> /var/spool/cron/root
@@ -255,7 +267,7 @@ init_rsyslog(){
 
 init_profile(){
     yum -y install wget
-    wget -O /etc/profile https://0vj6.github.io/sh/init/profile/profile
+    wget -O /etc/profile https://0vj6.github.io/sh/init/centos7/profile/profile
     chmod 644 /etc/profile
     echo "* * * * * chattr +a /tmp/.*timing" >> /var/spool/cron/root
     echo "* * * * * chattr +a /tmp/.*record" >> /var/spool/cron/root
--- init/init.ubuntu.20.04.sh
+++ init/init.ubuntu.20.04.sh
@@ -6,7 +6,7 @@
 #     https://cloud.google.com/solutions/chrome-desktop-remote-on-compute-engine
 #     https://remotedesktop.google.com/headless
 
-desktop(){
+init_desktop(){
     sudo apt update
     sudo apt -y upgrade
     sudo apt -y install gcc \
@@ -19,7 +19,7 @@ desktop(){
                         net-tools \
                         dos2unix \
                         tree \
-                        wget \
+                        sudo wget \
                         curl \
                         lrzsz \
                         openssh-server \
@@ -46,11 +46,19 @@ desktop(){
                         exfat-utils \
                         rar \
                         unrar \
-                        vino \
                         hydra \
                         sqlmap
     sudo apt -y install python-pip \
                         python-virtualenv
+    # Compile and use for FileZilla on Ubuntu 20.04 LTS
+    # sudo apt -y install nettle-dev \
+    #                     libghc-gnutls-dev \
+    #                     gettext \
+    #                     libwxgtk3.0-gtk3-dev \
+    #                     libpugixml-dev \
+    #                     libdbus-1-dev \
+    #                     libgtk-3-dev \
+    #                     libsqlite3-dev
     sudo apt -y install ubuntu-gnome-desktop \
                         ubuntu-desktop \
                         gnome-tweak-tool \
@@ -69,9 +77,11 @@ desktop(){
                         fcitx-frontend-gtk3 \
                         fcitx-libpinyin \
                         fcitx-libs \
-                        fcitx-module-kimpanel
-    sudo apt -y install shutter \
-                        fcitx-frontend-qt4
+                        fcitx-module-kimpanel \
+                        vino
+    # sudo apt -y install telegram-desktop
+    sudo apt -y install shutter
+    sudo apt -y install fcitx-frontend-qt4
     sudo apt -y autoremove --purge libreoffice* remmina* firefox* firefox-locale*
     sudo apt -y autoremove --purge libreoffice-base-core \
                                    libreoffice-calc \
@@ -90,41 +100,57 @@ desktop(){
                                    libreoffice-writer
     sudo apt -y autoremove --purge remmina
     sudo apt -y autoremove --purge firefox
-    # wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
-    # sudo dpkg --install mysql-apt-config_0.8.14-1_all.deb
+    # sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+    # sudo dpkg --install mysql-apt-config_0.8.15-1_all.deb
     # sudo apt update
     # sudo apt install --assume-yes --fix-broken
-    # rm -f mysql-apt-config_0.8.14-1_all.deb
-    # wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+    # sudo rm -f mysql-apt-config_0.8.15-1_all.deb
+    # sudo wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+    # sudo dpkg --install percona-release_latest.$(lsb_release -sc)_all.deb
+    # sudo apt update
+    # sudo apt install --assume-yes --fix-broken
+    # sudo rm -f percona-release_latest.$(lsb_release -sc)_all.deb
+    # sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
     # sudo dpkg --install chrome-remote-desktop_current_amd64.deb
     # sudo apt update
     # sudo apt install --assume-yes --fix-broken
-    # rm -f chrome-remote-desktop_current_amd64.deb
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    # sudo rm -f chrome-remote-desktop_current_amd64.deb
+    sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg --install google-chrome-stable_current_amd64.deb
     sudo apt update
     sudo apt install --assume-yes --fix-broken
-    rm -f google-chrome-stable_current_amd64.deb
-    wget https://repo.steampowered.com/steam/archive/precise/steam_latest.deb
+    sudo rm -f google-chrome-stable_current_amd64.deb
+    sudo wget https://repo.steampowered.com/steam/archive/precise/steam_latest.deb
     sudo dpkg --install steam_latest.deb
     sudo apt update
     sudo apt install --assume-yes --fix-broken
-    rm -f steam_latest.deb
+    sudo rm -f steam_latest.deb
     sudo apt -y install libextutils-depends-perl libextutils-pkgconfig-perl
-    wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
-    wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
-    wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
+    sudo wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
+    sudo wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
+    sudo wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
     sudo dpkg --install libgoocanvas-common_1.0.0-1_all.deb libgoocanvas3_1.0.0-1_amd64.deb libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
     sudo apt update
     sudo apt install --assume-yes --fix-broken
-    rm -f libgoocanvas-common_1.0.0-1_all.deb libgoocanvas3_1.0.0-1_amd64.deb libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
+    sudo rm -f libgoocanvas-common_1.0.0-1_all.deb libgoocanvas3_1.0.0-1_amd64.deb libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
     curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
     echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-    wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+    sudo apt update && sudo apt -y install signal-desktop
+    sudo wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
     sudo dpkg --install nordvpn-release_1.0.0_all.deb
     sudo apt update
     sudo apt install --assume-yes --fix-broken
-    sudo apt update && sudo apt -y install signal-desktop
+    sudo rm -f nordvpn-release_1.0.0_all.deb
+    # sudo wget https://repo.skype.com/latest/skypeforlinux-64.deb
+    # sudo dpkg --install skypeforlinux-64.deb
+    # sudo apt update
+    # sudo apt install --assume-yes --fix-broken
+    # sudo rm -f skypeforlinux-64.deb
+    sudo wget https://dl.discordapp.net/apps/linux/0.0.10/discord-0.0.10.deb
+    sudo dpkg --install discord-0.0.10.deb
+    sudo apt update
+    sudo apt install --assume-yes --fix-broken
+    sudo rm -f discord-0.0.10.deb
     sudo snap install telegram-desktop
     sudo snap install --classic skype
     sudo snap install --classic code
@@ -148,8 +174,63 @@ desktop(){
     git config --list --show-origin
 }
 
+init_server(){
+    sudo apt update
+    sudo apt -y upgrade
+    sudo apt -y install gcc \
+                        g++ \
+                        vim \
+                        git \
+                        mlocate \
+                        iftop \
+                        sysstat \
+                        net-tools \
+                        dos2unix \
+                        tree \
+                        sudo wget \
+                        curl \
+                        lrzsz \
+                        openssh-server \
+                        zip \
+                        unzip \
+                        make \
+                        traceroute \
+                        mtr \
+                        telnet \
+                        python3-pip \
+                        python3-virtualenv \
+                        lsb-release \
+                        ansible \
+                        cockpit \
+                        cockpit-ws \
+                        cockpit-dashboard \
+                        cockpit-storaged \
+                        ethtool \
+                        libpam-google-authenticator \
+                        extundelete \
+                        httping \
+                        nmap \
+                        whois \
+                        exfat-utils \
+                        rar \
+                        unrar \
+                        hydra \
+                        sqlmap
+    sudo apt -y install python-pip \
+                        python-virtualenv
+    sudo git config --global user.name "root"
+    sudo git config --global user.email root@localhost
+    sudo git config --list --show-origin
+    git config --global user.name "$USER"
+    git config --global user.email "$USER"@localhost
+    git config --list --show-origin
+}
+
 case $1 in
     desktop)
-        desktop
+        init_desktop
+    ;;
+    server)
+        init_server
     ;;
 esac
